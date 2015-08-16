5a244b2110b5e971e6875c0e6d9a5523421543b2
mmlevy <gx200df@aol.com>
2015-08-15 21:32:37 -0700

    Added folders and files for website skeleton to build onto.

---
+++ js/resumeBuilder.js
@@ -0,0 +1,3 @@
+var formattedName = HTMLheaderName.replace("%data%", "Marion Levy");
+var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
+$("#header").prepend(formattedName, formattedRole);
